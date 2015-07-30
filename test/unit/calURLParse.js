8ab13214c5139250cf1ada16b61f35c704add902
johnwquarles <john.w.quarles@gmail.com>
2015-07-30 16:33:47 -0500

    gitignore, added node-cal as an npm dependency and changed code to reflect that; no more subproject problems w/ git

--- test/unit/calURLParse.js
+++ test/unit/calURLParse.js
@@ -1,23 +1,24 @@
 var expect = require('chai').expect;
 var path = require('path');
 var parse = require(path.join(process.cwd(), '/lib/calURLParse'));
+var calpath = "node_modules/node-cal/node-cal.js";
 
 describe('calURLParse', function () {
   it('should handle the base /cal route', function () {
-    expect(parse('/cal')).to.equal('./node-cal/node-cal.js');
+    expect(parse('/cal')).to.equal(calpath);
   });
 
   it('should handle a full year route', function () {
-    expect(parse('/cal/2015')).to.equal('./node-cal/node-cal.js 2015');
-    expect(parse('/cal/9999999')).to.equal('./node-cal/node-cal.js 9999999');
+    expect(parse('/cal/2015')).to.equal(calpath + " 2015");
+    expect(parse('/cal/9999999')).to.equal(calpath + " 9999999");
   });
 
   it('should handle a month first route', function () {
-    expect(parse('/cal/1/2015')).to.equal('./node-cal/node-cal.js 1 2015');
+    expect(parse('/cal/1/2015')).to.equal(calpath + " 1 2015");
   });
 
   it('should handle a month last route', function () {
-    expect(parse('/cal/2015/1')).to.equal('./node-cal/node-cal.js 1 2015');
+    expect(parse('/cal/2015/1')).to.equal(calpath + " 1 2015");
   });
 
   it('should handle invalid routes', function () {
